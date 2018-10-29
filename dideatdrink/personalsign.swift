de4d0439ffbab1441074d8b8269cfd2b4b147b7f
mehmet selen <mehmetselen2@yandex.com>
2018-10-29 16:25:36 +0300

    ilk commit

---
+++ dideatdrink/personalsign.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dideatdrink
+//
+//  Created by Suat Selen on 29.10.2018.
+//  Copyright © 2018 mehmet selen. All rights reserved.
+//
+
+import UIKit
+
+class personalsign: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
